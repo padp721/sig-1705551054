54fb08db22948f4909e74e50182fb132a669ffce
randomize721 <anggadp91@gmail.com>
2020-03-12 23:06:35 +0800

    setting cleardb

--- conn.php
+++ conn.php
@@ -1,8 +1,15 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-    $db="leaflet";
+	//ClearDB
+	$servername = "us-cdbr-gcp-east-01.cleardb.net";
+	$username = "b70fae5594e1b1";
+	$password = "d51644a7";
+    $db="gcp_1cb724905c9d985afa41";
+
+	//Local
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+    // $db="leaflet";
     
     $conn = mysqli_connect($servername, $username, $password,$db);
 ?>
